0714f36385ebd177ed913d4bf0558cb2c967fcf1
habinezadalvan <45101716+habinezadalvan@users.noreply.github.com>
2019-07-31 18:41:27 +0200

    chore(configure postgres and sequelize): (#4)
    Instlled sequelize
    created src folder with config, migrations, models, seeds folders
    configured postgres
    created User model
    Configured babel
    
    [Finishes #167313387]
    [Finishes #167595103]

---
+++ src/config/config.js
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_NAME,
+  DB_NAME_TEST,
+  HOST
+} = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: HOST,
+    dialect: 'postgres',
+    port: DB_PORT,
+    seederStorage: 'sequelize'
+  },
+  test: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME_TEST,
+    host: HOST,
+    dialect: 'postgres',
+    port: DB_PORT,
+    seederStorage: 'sequelize'
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: HOST,
+    dialect: 'postgres',
+    port: DB_PORT,
+    seederStorage: 'sequelize'
+  }
+};
