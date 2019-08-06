535c998a80e684f001788ab7efa01fc7c26d5e70
nshutijonathan <nshutijonathan130@gmail.com>
2019-08-06 17:10:36 +0200

    feature(code-refactoring):fixed-eslint[Finish:167313396]

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,36 +1,36 @@
 import env from 'dotenv';
 import jwt from 'jsonwebtoken';
+
 env.config();
 const validateToken = (req, res, next) => {
-    let token = req.headers['x-access-token'] || req.headers.authorization;
-    if (!token) {
+  let token = req.headers['x-access-token'] || req.headers.authorization;
+  if (!token) {
+    return res.status(401).send({
+      status: 401,
+      message: 'Unauthorized access'
+    });
+  }
+  if (token.startsWith('Bearer ')) {
+    token = token.slice(7, token.length);
+  }
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decode) => {
+      if (err) {
         return res.status(401).send({
-            status:401,
-            message:'Unauthorized access'
-        })
-    }
-    if (token.startsWith('Bearer ')) {
-      token = token.slice(7, token.length);
-    }
-    if (token) {
-      jwt.verify(token, process.env.SECRET_KEY, (err, decode) => {
-        if (err) {
-            return res.status(401).send({
-                status:401,
-                message:'Token is not valid'
-            })
-        } else {
-          req.auth = decode;
-          next();
-        }
-        return decode;
-        
-      });
-    } else {
-        return res.status(400).send({
-            status:400,
-            message:'Auth token is not supplied'
-        }) 
-    }
-  };
-  export default validateToken;
+          status: 401,
+          message: 'Token is not valid'
+        });
+      }
+      req.auth = decode;
+      next();
+
+      return decode;
+    });
+  } else {
+    return res.status(400).send({
+      status: 400,
+      message: 'Auth token is not supplied'
+    });
+  }
+};
+export default validateToken;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,69 +1,134 @@
+import { Op } from 'sequelize';
 import database from '../models/index';
-import { Op } from 'sequelize'
 
+/**
+ *
+ *
+ * @class UserService
+ */
 class UserService {
+  /**
+   *
+   *
+   * @static
+   * @param {*} email
+   * @param {*} username
+   * @returns
+   * @memberof UserService
+   * @returns {Object} return db result object
+   */
   static async findOne(email, username) {
     try {
       return await database.user.findOne({
-        where: { [Op.or]: [{ email }, {username: username}]}
+        where: { [Op.or]: [{ email }, { username }] }
       });
     } catch (error) {
       throw error;
     }
   }
- static async getAllUsers() {
-   try {
-     return await database.user.findAll();
-   } catch (error) {
-     throw error;
-   }
- }
- static async addUser(newUser) {
-   try {
-     return await database.user.create(newUser);
-   } catch (error) {
-     throw error;
-   }
- }
- static async getOneUser(id) {
-   try {
-     const theUser = await database.user.findOne({
-       where: { id: Number(id) }
-     });
-     return theUser;
-   } catch (error) {
-     throw error;
-   }
- }
- static async deleteUser(id) {
-   try {
-     const UserToDelete = await database.user.findOne({ where: { id: Number(id) } });
-     if (UserToDelete) {
-       const deletedUser = await database.user.destroy({
-         where: { id: Number(id) }
-       });
-       return deletedUser;
-     }
-     return null;
-   } catch (error) {
-     throw error;
-   }
- }
- static async updateUser(email, updateUser) {
-  try {
-    const userToUpdate = await database.user.findOne({
-      where: { email: email }
-    });
 
-    if (userToUpdate) {
-      await database.user.update(updateUser, { where: { email: email } });
-
-      return updateBook;
+  /**
+   *
+   *
+   * @static
+   * @returns
+   * @memberof UserService
+   * @returns {Object} return db result object
+   */
+  static async getAllUsers() {
+    try {
+      return await database.user.findAll();
+    } catch (error) {
+      throw error;
     }
-    return null;
-  } catch (error) {
-    throw error;
   }
-}
+
+  /**
+   *
+   *
+   * @static
+   * @param {*} newUser
+   * @returns
+   * @memberof UserService
+   * @returns {Object} return db result object
+   */
+  static async addUser(newUser) {
+    try {
+      return await database.user.create(newUser);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {*} id
+   * @returns
+   * @memberof UserService
+   * @returns {Object} return db result object
+   */
+  static async getOneUser(id) {
+    try {
+      const theUser = await database.user.findOne({
+        where: { id: Number(id) }
+      });
+      return theUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {*} id
+   * @returns
+   * @memberof UserService
+   * @returns {Object} return db result object
+   */
+  static async deleteUser(id) {
+    try {
+      const UserToDelete = await database.user.findOne({ where: { id: Number(id) } });
+      if (UserToDelete) {
+        const deletedUser = await database.user.destroy({
+          where: { id: Number(id) }
+        });
+        return deletedUser;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {*} email
+   * @param {*} updateUser
+   * @returns
+   * @memberof UserService
+   * @returns {Object} return db result object
+   */
+  static async updateUser(email, updateUser) {
+    try {
+      const userToUpdate = await database.user.findOne({
+        where: { email }
+      });
+
+      if (userToUpdate) {
+        await database.user.update(updateUser, { where: { email } });
+
+        return updateUser;
+      }
+      return null;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 export default UserService;
