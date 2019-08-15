75ef00e5339aac9625eeb8ca690d6d6e41e5cc66
Noah Kalyesubula <noahkalyesubula@Noahs-MacBook-Pro.local>
2019-08-15 15:43:29 +0200

    feature(follow-users):Improve follow user tests[Finishes #167313409]

--- test/test-follow.js
+++ test/test-follow.js
@@ -83,4 +83,30 @@ describe('test for following and unfollowing a user', () => {
         done();
       });
   });
+  it('test for a non existing user ', (done) => {
+    chai.request(server)
+      .post('/api/v1/users/profiles/199990/follow')
+      .send({})
+      .set('Authorization', usertoken)
+      .end((error, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message');
+        expect(res.body.message).to.contain('user does not exist');
+        done();
+      });
+  });
+  it('test for user not being able to follow himself', (done) => {
+    chai.request(server)
+      .post('/api/v1/users/profiles/3/follow')
+      .send({})
+      .set('Authorization', usertoken)
+      .end((error, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message');
+        expect(res.body.message).to.contain('You cannot follow yourself');
+        done();
+      });
+  });
 });
