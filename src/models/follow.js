4f0b5e4060b432352ac3985b7583a602ada05cbc
nkalyesubula <49583916+nkalyesubula@users.noreply.github.com>
2019-08-15 17:51:20 +0200

    #167313409 Users should be able to follow each other (#20)
    * rebase
    
    * feature(follow-users):Users should be able to follow each other [Finishes #167313409]
    
    * feature(follow-users):Improve follow user tests[Finishes #167313409]

--- src/models/follow.js
+++ src/models/follow.js
@@ -1,21 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Follow = sequelize.define('Follow', {
-    followerId: DataTypes.INTEGER,
-    followedUserId: DataTypes.INTEGER
-  }, {});
-  Follow.associate = ({ user }) => {
-    // associations can be defined here
-    Follow.belongsTo(user, {
-      foreignKey: 'followerId',
-      onDelete: 'CASCADE',
-      as: 'followerDetails'
-    });
-    Follow.belongsTo(user, {
-      foreignKey: 'followedUserId',
-      onDelete: 'CASCADE',
-      as: 'authorDetails'
-    });
-  };
-  return Follow;
+      const Follow = sequelize.define('Follow', {
+            followerId: DataTypes.INTEGER,
+            followedUserId: DataTypes.INTEGER
+      }, {});
+      Follow.associate = ({ user }) => {
+            // associations can be defined here
+            Follow.belongsTo(user, {
+                  foreignKey: 'followerId',
+                  onDelete: 'CASCADE',
+                  as: 'followerDetails'
+            });
+            Follow.belongsTo(user, {
+                  foreignKey: 'followedUserId',
+                  onDelete: 'CASCADE',
+                  as: 'authorDetails'
+            });
+      };
+      return Follow;
 };
