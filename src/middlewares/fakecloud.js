d5bc09ec2593e563c50b5baced622e1170fff39b
habinezadalvan <45101716+habinezadalvan@users.noreply.github.com>
2019-08-21 14:31:53 +0200

    bug(fixing create article mocking tests):Only cloudinary function is mocked instead of the whole creating article route [Finishes #168000401] (#31)

--- src/middlewares/fakecloud.js
+++
@@ -1,9 +0,0 @@
-import fakeUpload from '../../test/mock/mock.cloud';
-
-const fakeCloud = (req, res, next) => {
-  req.auth = fakeUpload.auth;
-  req.body = fakeUpload.body;
-  req.files = fakeUpload.files;
-  next();
-};
-export default fakeCloud;
