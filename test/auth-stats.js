701e54871c82bc5f1e1719880de7a8d3ed3f2247
anguandia mike <mikeanguandia@gmail.com>
2019-08-30 09:56:37 +0200

    #167313420 A user should be able to see their reading statistics (#49)
    * 167313420 a user should be able to view their reading stats
    
    [WIP #167313420]
    
    * 167313420 user should be able to view their stats
    
    [(Needs review) #167313420]
    
    * rebase and incorporate latest changes
    
    * 167313420 rebase and rename stats test to run before other stats populating test request
    
    * resolve conflict in articles controller
    
    * reolve conflict in articles ctrler
    
    * reolve conflict in articles ctrler
    
    * resolve merge conflict

---
+++ test/auth-stats.js
@@ -0,0 +1,67 @@
+import { chai, expect, server } from './test-setup';
+
+let token;
+describe('Test user stats', () => {
+  before((done) => {
+    chai.request(server)
+      .post('/api/v1/users/login')
+      .send({ email: 'admin@gmail.com', password: 'ASqw12345' })
+      .end((error, res) => {
+        token = `Bearer ${res.body.token}`;
+        done();
+      });
+  });
+  describe('test getting stats', () => {
+    it('should return correct response if none', (done) => {
+      chai.request(server)
+        .get('/api/v1/users/stats')
+        .set('Authorization', token)
+        .end((err, res) => {
+          expect(res.status).to.be.equal(404);
+          expect(res.body.message).to.include('not found');
+          done();
+        });
+    });
+    it('should return correct response if user not logged in', (done) => {
+      chai.request(server)
+        .get('/api/v1/users/stats')
+        .end((err, res) => {
+          expect(res.status).to.be.equal(400);
+          expect(res.body.message).to.include('not logged in');
+          done();
+        });
+    });
+  });
+  describe('test stats gathering', () => {
+    it('should not populate stats if user unauthenticated', (done) => {
+      chai.request(server)
+        .get('/api/v1/articles/fakeslug2')
+        .end(() => {
+          chai.request(server)
+            .get('/api/v1/users/stats')
+            .set('Authorization', token)
+            .end((err, res) => {
+              expect(res.status).to.be.equal(404);
+              expect(res.body.message).to.include('not found');
+              done();
+            });
+        });
+    });
+    it('should log every read', (done) => {
+      chai.request(server)
+        .get('/api/v1/articles/fakeslug2')
+        .set('Authorization', token)
+        .end(() => {
+          chai.request(server)
+            .get('/api/v1/users/stats')
+            .set('Authorization', token)
+            .end((err, res) => {
+              expect(res.status).to.be.equal(200);
+              expect(res.body).to.have.deep.property('message', 'your reading stats');
+              expect(res.body.data).to.be.a('Array');
+              done();
+            });
+        });
+    });
+  });
+});
