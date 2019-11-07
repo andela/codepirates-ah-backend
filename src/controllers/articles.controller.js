b80c8781614358e7184c983b8e9ba091478ffd67
Anguandia <mikeanguandia@gmail.com>
2019-11-07 15:52:40 +0200

    share front, insteat of back end link

--- src/controllers/articles.controller.js
+++ src/controllers/articles.controller.js
@@ -120,8 +120,8 @@ class Articles {
         try {
           const userDetails = await Userservice.getOneUser(article.authorId);
           const {
-            username, firstname, lastname, image
-          } = userDetails;
+ username, firstname, lastname, image 
+} = userDetails;
           const user = {
             username,
             firstname,
@@ -264,8 +264,8 @@ class Articles {
     ]);
     const userDetails = await Userservice.getOneUser(article.authorId);
     const {
-      username, firstname, lastname, image
-    } = userDetails;
+ username, firstname, lastname, image 
+} = userDetails;
     const user = {
       username,
       firstname,
@@ -373,8 +373,8 @@ class Articles {
       return util.send(res);
     }
     const {
-      title, body, description, images
-    } = req.body;
+ title, body, description, images 
+} = req.body;
     const updatedArticle = await articleService.updateArticle(req.params.slug, {
       title,
       body,
@@ -404,8 +404,7 @@ class Articles {
       util.setError(404, 'Article is not found.');
       return util.send(res);
     }
-    const location = `${process.env.BACKEND_URL}/api/${process.env.API_VERSION}`;
-    const url = `${location}/articles/${req.params.slug}`;
+    const { url } = req.body;
     switch (req.params.channel) {
       case 'facebook':
         await OpenUrlHelper.openUrl(
