199c9d58adcd21bd18b8b8392235ad6b2b0159d9
nkalyesubula <49583916+nkalyesubula@users.noreply.github.com>
2019-11-11 11:12:00 +0200

    #169643311  Users should be able to get notifications (#76)

--- src/controllers/notifications.controller.js
+++ src/controllers/notifications.controller.js
@@ -1,5 +1,8 @@
 import notificationHelper from '../helpers/notification.helper';
+import models from '../models/index';
+import Util from '../helpers/util';
 
+const util = new Util();
 
 /**
  * @description Contains method to allow users opt in and out of notifications
@@ -29,6 +32,65 @@ class NotificationController {
         break;
       default:
         return res.status(404).json({ error: 'route does not exist' });
+    }
+  }
+
+  /**
+ *
+ * @description Method to opt in and out of email and push notication
+ * @static
+ * @param {object} req client request
+ * @param {object} res server response
+ * @returns {Object} server response object
+ * @param  {Function} next passes control to the next middleware
+ * @memberof NotificationController
+ */
+  static async getUserNotification(req, res) {
+    try {
+      const user = req.auth;
+      const userNotification = await models.AppNotification.findAll({
+        where: { receiverId: user.id },
+      });
+      if (userNotification.length === 0) {
+        util.setError(404, 'You currently do not have notifications');
+        return util.send(res);
+      }
+      util.setSuccess(200, 'notifications', userNotification);
+      return util.send(res);
+    } catch (error) {
+      util.setError(400, { error: error.message });
+      return util.send(res);
+    }
+  }
+
+  /**
+ *
+ * @description Method to change notification status to true
+ * @static
+ * @param {object} req client request
+ * @param {object} res server response
+ * @returns {Object} server response object
+ * @param  {Function} next passes control to the next middleware
+ * @memberof NotificationController
+ */
+  static async updateNotificationStatus(req, res) {
+    try {
+      const { id } = req.params;
+      const userNotification = await models.AppNotification.findOne({
+        where: { id }
+      });
+      if (userNotification) {
+        await models.AppNotification.update({
+          read: true,
+        }, { where: { id } });
+        util.setSuccess(200, 'Notification has been retrieved', null);
+        return util.send(res);
+      }
+      util.setError(404, { message: 'Notification Not Found' });
+      return util.send(res);
+    } catch (error) {
+      util.setError(400, { error: error.message });
+      return util.send(res);
     }
   }
 }
