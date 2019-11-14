16e8bec2d2f929103a6a5799c15e06b8fda12c0a
Minega Shyaka Patrick <minega25@gmail.com>
2019-11-14 11:32:57 +0200

    bug(tags): search by tags (#80)

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -1,10 +1,10 @@
-import _ from 'lodash';
-import moment from 'moment';
-import ArticleModel from '../services/article.service';
-import UserModel from '../services/user.service';
-import TagModel from '../services/tag.service';
-import Util from '../helpers/util';
-import Helper from '../helpers/helper';
+import _ from "lodash";
+import moment from "moment";
+import ArticleModel from "../services/article.service";
+import UserModel from "../services/user.service";
+import TagModel from "../services/tag.service";
+import Util from "../helpers/util";
+import Helper from "../helpers/helper";
 
 const util = new Util();
 /**
@@ -21,61 +21,79 @@ class Search {
    * @param {*} res
    * @return {Json} return json object
    * @memberof Search
-  */
+   */
   static async processSearchQuery(req, res) {
     const { q, offset, limit } = req.query;
     const searchQuery = JSON.parse(q);
-    const articles = await ArticleModel.searchArticle(searchQuery.keyword, offset, limit);
+    const articles = await ArticleModel.searchArticle(
+      searchQuery.keyword,
+      offset,
+      limit
+    );
     const foundArticles = _.map(
       articles,
       _.partialRight(_.pick, [
-        'authorId',
-        'slug',
-        'title',
-        'description',
-        'body',
-        'taglist',
-        'favorited',
-        'favoritedcount',
-        'flagged',
-        'images',
-        'views',
-        'createdAt',
+        "authorId",
+        "slug",
+        "title",
+        "description",
+        "body",
+        "taglist",
+        "favorited",
+        "favoritedcount",
+        "flagged",
+        "images",
+        "views",
+        "createdAt"
       ])
     );
 
-    await Promise.all(foundArticles.map(async (article) => {
-      try {
-        const userDetails = await UserModel.getOneUser(article.authorId);
-        const { username, image } = userDetails;
-        const readTime = Helper.calculateReadTime(article.body);
-        const timeAgo = moment(article.createdAt).fromNow();
-        article.readtime = readTime;
-        article.username = username;
-        article.userImage = image;
-        article.timeCreated = timeAgo;
-        return true;
-      } catch (error) {
-        console.log(error);
+    await Promise.all(
+      foundArticles.map(async article => {
+        try {
+          const userDetails = await UserModel.getOneUser(article.authorId);
+          const { username, image } = userDetails;
+          const readTime = Helper.calculateReadTime(article.body);
+          const timeAgo = moment(article.createdAt).fromNow();
+          article.readtime = readTime;
+          article.username = username;
+          article.userImage = image;
+          article.timeCreated = timeAgo;
+          return true;
+        } catch (error) {
+          console.log(error);
+        }
+      })
+    );
+    let duplicateFoundTags = [];
+    foundArticles.map(article => {
+      const articleTags = article.taglist;
+      const found = articleTags.filter(
+        articleTag => articleTag === searchQuery.tag
+      );
+      if (found.length >= 1) {
+        for (let i = 0; i <= found.length - 1; i++) {
+          duplicateFoundTags = [found[i], ...duplicateFoundTags];
+        }
       }
-    }));
+    });
+
+    let foundTags = duplicateFoundTags.filter(
+      (item, index) => duplicateFoundTags.indexOf(item) === index
+    );
+    if (foundTags[0] === "") {
+      foundTags = [];
+    }
     const users = await UserModel.searchUser(searchQuery.user, offset, limit);
     const foundUsers = _.map(
       users,
-      _.partialRight(_.pick, [
-        'username',
-        'image',
-        'bio',
-      ])
+      _.partialRight(_.pick, ["username", "image", "bio"])
     );
-    const tags = await TagModel.searchTag('Tag', searchQuery.tag, offset, limit);
-    const foundTags = _.map(
-      tags,
-      _.partialRight(_.pick, [
-        'name',
-      ])
-    );
-    util.setSuccess(200, 'Search successful', { foundArticles, foundUsers, foundTags });
+    util.setSuccess(200, "Search successful", {
+      foundArticles,
+      foundUsers,
+      foundTags
+    });
     return util.send(res);
   }
 }
